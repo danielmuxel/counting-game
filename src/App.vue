7c550e040ca1a7aa14adb05fe515e588d742763b
Daniel Muxel <daniel.muxel@gmail.com>
2022-04-29 21:36:54 +0200

    added a nicer gui

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="min-h-screen flex justify-center flex-col bg-gray-400">
+  <div class="min-h-screen flex justify-center flex-col bg-gray-300">
     <div class="container">
-      <v-stage class="mx-auto bg-white" :config="configKonva">
-        <v-layer>
-          <v-group v-for="(group, index) in groups" :key="`groups-${index}`" :config="{ ...group.position }">
-            <v-circle :config="group.circle">
-            </v-circle>
-          </v-group>
-        </v-layer>
-      </v-stage>
+      <div class="flex justify-center mx-auto">
+        <div class="relative">
+          <v-stage class="shadow-lg bg-white rounded-lg" :config="configKonva">
+            <v-layer>
+              <v-group v-if="showCircles" v-for="(group, index) in groups" :key="`groups-${index}`"
+                :config="{ ...group.position }">
+                <v-circle :config="group.circle">
+                </v-circle>
+              </v-group>
+            </v-layer>
+          </v-stage>
+          <div v-show="showAnswers"
+            class="absolute bottom-0 left-0 right-0 top-0 flex flex-col justify-center p-16 bg-red-500 rounded-lg">
+            <h1 v-if="!resultMessage" class="text-center text-4xl text-gray-50 mb-24">Wieviele Kreise konntest du
+              zählen?</h1>
+            <h1 v-if="resultMessage" class="text-center text-4xl text-gray-50 mb-24">{{ resultMessage }}</h1>
+            <div class="flex justify-between">
+              <button v-for="i in 10 " :key="`answer-${i}`" @click="selectAnswer(i)" :disabled="selectedAnswer"
+                :class="{ 'bg-green-500 border-green-700 border-2 text-green-900': selectedAnswer && selectedAnswer === correctAnswer && i === correctAnswer, 'bg-yellow-500 border-yellow-700 border-2 text-yellow-900': selectedAnswer && selectedAnswer !== correctAnswer && i === correctAnswer }"
+                class="bg-gray-200 py-2 px-4 relative h-16 w-16 text-2xl font-bold rounded">
+                <span class="absolute inset-0 flex justify-center items-center">{{ i }}</span>
+              </button>
+            </div>
+            <div class="flex justify-center items-center mt-24">
+              <button @click="restart" class="flex items-center bg-gray-200 py-2 px-4 rounded font-bold">
+                <i class="fa-solid fa-arrow-rotate-right mr-3"></i>
+                Neustart
+              </button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -29,43 +56,40 @@ export default {
         fill: "red",
         stroke: "black",
         strokeWidth: 4
-      }
+      },
+      showCircles: true,
+      showAnswers: false,
+      selectedAnswer: null,
+      resultMessage: null
     };
   },
   methods: {
     generateCircles(amount) {
       for (let i = 0; i < amount; i++) {
         let count = 0;
-
         const radius = 50;
         const minDistance = 4 * radius * radius;
-
-        let positionX
-        let positionY
+        let positionX;
+        let positionY;
         let isColliding = false;
-
         do {
           count++;
-
           // 1280 -> radius <-
-          positionX = this.getRandomInt(this.configKonva.width - radius * 3) + radius
-          positionY = this.getRandomInt(this.configKonva.height - radius * 3) + radius
-
+          let gap = radius + 10;
+          positionX = this.getRandomInt(this.configKonva.width - gap * 3) + gap;
+          positionY = this.getRandomInt(this.configKonva.height - gap * 3) + gap;
           for (let j = 0; j < this.groups.length; j++) {
             const group = this.groups[j];
-
-            let distance = (group.position.x - positionX) ** 2 + (group.position.y - positionY) ** 2
-
+            let distance = (group.position.x - positionX) ** 2 + (group.position.y - positionY) ** 2;
             if (distance < minDistance) {
-              isColliding = true
-              break
-            } else {
-              isColliding = false
+              isColliding = true;
+              break;
+            }
+            else {
+              isColliding = false;
             }
           }
-
-        } while (isColliding)
-
+        } while (isColliding);
         this.groups.push({
           position: {
             x: positionX,
@@ -82,13 +106,45 @@ export default {
     },
     getRandomInt(max) {
       return Math.floor(Math.random() * max);
+    },
+    startTest() {
+      const amount = this.getRandomInt(10) + 1;
+      this.generateCircles(amount);
+      setTimeout(() => {
+        this.showCircles = false;
+        this.showAnswers = true;
+      }, 1000);
+    },
+    selectAnswer(answer) {
+      this.selectedAnswer = answer;
+      this.getGameResult();
+    },
+    getGameResult() {
+      const correctAnswer = this.groups.length;
+      if (this.selectedAnswer === correctAnswer) {
+        this.resultMessage = "Richtig, die Antwort war " + correctAnswer;
+      }
+      else {
+        this.resultMessage = "Leider falsch, die Antwort war " + correctAnswer;
+      }
+    },
+    restart() {
+      this.showCircles = true;
+      this.showAnswers = false;
+      this.selectedAnswer = null;
+      this.resultMessage = null;
+      this.groups = [];
+      this.startTest();
     }
   },
   created() {
-    const amount = this.getRandomInt(10) + 1;
-    this.generateCircles(amount);
-    console.log('groups', this.groups);
-  }
+    this.startTest();
+  },
+  computed: {
+    correctAnswer() {
+      return this.groups.length;
+    }
+  },
 };
 
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import VueKonva from "vue-konva";
 import './assets/css/tailwind.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App);
 app.use(VueKonva);
